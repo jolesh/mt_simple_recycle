ebd281b2d03cb431d0efce34fd9bff65299f39a4
jolesh <joraloe@gmail.com>
2022-09-20 17:22:01 +0200

    Added translations
    Added translations for Danish, German, Dutch and Esperanto. Also added a title in the mod.conf and improved the English description for nuggets.

--- register_items.lua
+++ register_items.lua
@@ -1,26 +1,28 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
 --Metal Nuggets
 
 minetest.register_craftitem("simple_recycle:steel_nugget", {
-   description = "A small piece of steel",
-   inventory_image = "simple_recycle_steel_nugget.png",
+   description = S("Steel Nugget"),
+   inventory_image = "simple_recycle_steel_nugget.png"
 })
 
 minetest.register_craftitem("simple_recycle:bronze_nugget", {
-   description = "A small piece of bronze",
-   inventory_image = "simple_recycle_bronze_nugget.png",
+   description = S("Bronze Nugget"),
+   inventory_image = "simple_recycle_bronze_nugget.png"
 })
 
 minetest.register_craftitem("simple_recycle:gold_nugget", {
-   description = "A small piece of gold",
-   inventory_image = "simple_recycle_gold_nugget.png",
+   description = S("Gold Nugget"),
+   inventory_image = "simple_recycle_gold_nugget.png"
 })
 
 minetest.register_craftitem("simple_recycle:tin_nugget", {
-   description = "A small piece of tin",
-   inventory_image = "simple_recycle_tin_nugget.png",
+   description = S("Tin Nugget"),
+   inventory_image = "simple_recycle_tin_nugget.png"
 })
 
 minetest.register_craftitem("simple_recycle:copper_nugget", {
-   description = "A small piece of copper",
-   inventory_image = "simple_recycle_copper_nugget.png",
+   description = S("Copper Nugget"),
+   inventory_image = "simple_recycle_copper_nugget.png"
 })
